aa780ddf4cd248b65db8924d5d23dd58aa5d7e83
Andrew Colannino <akcolannino@email.neit.edu>
2020-05-17 01:13:01 -0400

    framerate wip

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 mod rolly_game;
 use rolly_game::RollyGame;
 
-use quicksilver::{run, Graphics, Input, Result, Settings, Window};
+use quicksilver::{run, Graphics, Input, Result, Settings, Timer, Window};
 
 async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
 	let mut game = RollyGame::new();
+	let mut update_timer = Timer::time_per_second(60.0);
+	let mut draw_timer = Timer::time_per_second(60.0);
 
 	loop {
 		while let Some(_) = input.next_event().await {}
 
-		game.update(&input);
-		game.draw(&mut gfx);
+		while update_timer.tick() {
+			let delta_time = update_timer.remaining();
+			if let Some(delta_time) = delta_time {
+				println!("dt: {:?}", delta_time.as_secs_f32());
+			}
+		}
 
-		gfx.present(&window)?;
+		if draw_timer.tick() {
+			game.update(&input);
+			game.draw(&mut gfx);
+			gfx.present(&window)?;
+		}
 	}
 }
 
--- src/rolly_game.rs
+++ src/rolly_game.rs
@@ -31,25 +31,27 @@ impl RollyGame {
 	}
 
 	pub fn update(&mut self, input: &Input) {
-		if input.key_down(Key::Space) {
-			self.reset();
+		for _x in 0..900000 {
+			if input.key_down(Key::Space) {
+				self.reset();
+			}
+
+			const SPEED: f32 = 2.0;
+			// const GRAVITY: f32 = 6.0;
+
+			// TODO: DELTA_TIME
+			self.player.vel.x = 0.0;
+			// self.player.vel.y += GRAVITY;
+
+			if input.key_down(Key::A) {
+				self.player.vel.x -= SPEED;
+			}
+			if input.key_down(Key::D) {
+				self.player.vel.x += SPEED;
+			}
+
+			self.player.pos += self.player.vel;
 		}
-
-		const SPEED: f32 = 2.0;
-		const GRAVITY: f32 = 6.0;
-
-		// TODO: DELTA_TIME
-		self.player.vel.x = 0.0;
-		self.player.vel.y += GRAVITY;
-
-		if input.key_down(Key::A) {
-			self.player.vel.x -= SPEED;
-		}
-		if input.key_down(Key::D) {
-			self.player.vel.x += SPEED;
-		}
-
-		self.player.pos += self.player.vel;
 	}
 
 	pub fn draw(&mut self, gfx: &mut Graphics) {
