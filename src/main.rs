80d5fab5eb2ae4cf6052a9c7be93791a8b509d9b
Andrew Colannino <akcolannino@email.neit.edu>
2020-05-16 20:04:39 -0400

    refactored into RollyGame struct

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,64 @@
 // Example 8: Input
 // Respond to user keyboard and mouse input onscreen
 use quicksilver::{
-	geom::{Circle, Rectangle, Vector},
+	geom::{Rectangle, Vector},
 	graphics::Color,
 	input::Key,
 	run, Graphics, Input, Result, Settings, Window,
 };
 
-struct Player {
-	pos: Vector
+// TODO: put the game in the app and shake it all up
+struct RollyGame {
+	player: Player,
 }
 
-async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
-	// Keep track of the position of the square
-	let mut player = Player { pos: (300, 300).into() };
+struct Player {
+	pos: Vector,
+}
 
-	loop {
-		while let Some(_) = input.next_event().await {}
-		// Check the state of the keys, and move the square accordingly
+impl RollyGame {
+	pub fn new() -> Self {
+		let player = Player {
+			pos: (300, 300).into(),
+		};
+		RollyGame { player }
+	}
+
+	pub fn update(&mut self, input: &Input) {
 		const SPEED: f32 = 2.0;
 		if input.key_down(Key::A) {
-			player.pos.x -= SPEED;
+			self.player.pos.x -= SPEED;
 		}
 		if input.key_down(Key::D) {
-			player.pos.x += SPEED;
+			self.player.pos.x += SPEED;
 		}
 		if input.key_down(Key::W) {
-			player.pos.y -= SPEED;
+			self.player.pos.y -= SPEED;
 		}
 		if input.key_down(Key::S) {
-			player.pos.y += SPEED;
+			self.player.pos.y += SPEED;
 		}
+	}
 
+	pub fn draw(&mut self, gfx: &mut Graphics) {
 		gfx.clear(Color::from_hex("ade7ff"));
 		// Paint a blue square at the given position
 		gfx.fill_rect(
-			&Rectangle::new(player.pos, Vector::new(64.0, 64.0)),
+			&Rectangle::new(self.player.pos, Vector::new(64.0, 64.0)),
 			Color::BLUE,
 		);
-		// Paint a red square at the mouse position
-		gfx.fill_circle(&Circle::new(input.mouse().location(), 32.0), Color::RED);
+	}
+}
+
+async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+	let mut game = RollyGame::new();
+
+	loop {
+		while let Some(_) = input.next_event().await {}
+
+		game.update(&input);
+		game.draw(&mut gfx);
+
 		gfx.present(&window)?;
 	}
 }
