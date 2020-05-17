0a7dbe6ce28694b575fff27db54e5e61ce907801
Andrew Colannino <akcolannino@email.neit.edu>
2020-05-17 00:56:18 -0400

    example on 0.3.x

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,46 @@
-mod rolly_game;
-use rolly_game::RollyGame;
+// Draw some multi-colored geometry to the screen
+extern crate quicksilver;
 
-use quicksilver::{run, Graphics, Input, Result, Settings, Window};
+use quicksilver::{
+	geom::{Circle, Line, Rectangle, Transform, Triangle, Vector},
+	graphics::{Background::Col, Color},
+	lifecycle::{run, Settings, State, Window},
+	Result,
+};
 
-async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
-	let mut game = RollyGame::new();
+struct DrawGeometry;
 
-	loop {
-		while let Some(_) = input.next_event().await {}
+impl State for DrawGeometry {
+	fn new() -> Result<DrawGeometry> {
+		Ok(DrawGeometry)
+	}
 
-		game.update(&input);
-		game.draw(&mut gfx);
-
-		gfx.present(&window)?;
+	fn draw(&mut self, window: &mut Window) -> Result<()> {
+		window.clear(Color::WHITE)?;
+		window.draw(&Rectangle::new((100, 100), (32, 32)), Col(Color::BLUE));
+		window.draw_ex(
+			&Rectangle::new((400, 300), (32, 32)),
+			Col(Color::BLUE),
+			Transform::rotate(45),
+			10,
+		);
+		window.draw(&Circle::new((400, 300), 100), Col(Color::GREEN));
+		window.draw_ex(
+			&Line::new((50, 80), (600, 450)).with_thickness(2.0),
+			Col(Color::RED),
+			Transform::IDENTITY,
+			5,
+		);
+		window.draw_ex(
+			&Triangle::new((500, 50), (450, 100), (650, 150)),
+			Col(Color::RED),
+			Transform::rotate(45) * Transform::scale((0.5, 0.5)),
+			0,
+		);
+		Ok(())
 	}
 }
 
 fn main() {
-	run(
-		Settings {
-			title: "Input Example",
-			size: (854, 480).into(),
-			..Settings::default()
-		},
-		app,
-	);
+	run::<DrawGeometry>("Draw Geometry", Vector::new(800, 600), Settings::default());
 }
