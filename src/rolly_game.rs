02019f8ac4707ead778a619021ddeb1054aafb2c
Andrew Colannino <akcolannino@email.neit.edu>
2020-05-16 20:14:41 -0400

    player is cute lil ball

--- src/rolly_game.rs
+++ src/rolly_game.rs
@@ -1,5 +1,5 @@
 use quicksilver::{
-	geom::{Rectangle, Vector},
+	geom::{Circle, Vector},
 	graphics::Color,
 	input::Key,
 	Graphics, Input,
@@ -11,12 +11,14 @@ pub struct RollyGame {
 
 struct Player {
 	pos: Vector,
+	radius: f32,
 }
 
 impl RollyGame {
 	pub fn new() -> Self {
 		let player = Player {
 			pos: (300, 300).into(),
+			radius: 16.0,
 		};
 		RollyGame { player }
 	}
@@ -39,10 +41,10 @@ impl RollyGame {
 
 	pub fn draw(&mut self, gfx: &mut Graphics) {
 		gfx.clear(Color::from_hex("ade7ff"));
-		// Paint a blue square at the given position
-		gfx.fill_rect(
-			&Rectangle::new(self.player.pos, Vector::new(64.0, 64.0)),
-			Color::BLUE,
+
+		gfx.fill_circle(
+			&Circle::new(self.player.pos, self.player.radius),
+			Color::from_hex("4f30d9"),
 		);
 	}
 }
