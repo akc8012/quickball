af6a79ffeb38054f18f3975e089c9c16306bef93
Andrew Colannino <akcolannino@email.neit.edu>
2020-05-30 00:20:28 -0400

    stepper wip

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ use quicksilver::{
 };
 
 async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
-	let mut gameloop = GameRunner::new();
+	let mut gameloop = GameTimerStepper::new();
 
 	let mut running = true;
-	let mut next_pressed;
+	let mut _next_pressed;
 
 	while running {
-		next_pressed = false;
+		_next_pressed = false;
 
 		while let Some(event) = input.next_event().await {
 			if let Event::KeyboardInput(key) = event {
@@ -24,54 +24,39 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
 					running = false
 				}
 				if key.key() == Key::N && key.is_down() {
-					next_pressed = true
+					_next_pressed = true
 				}
 			}
 		}
 
-		gameloop.run(next_pressed, &input, &mut gfx, &window)?;
+		gameloop.step(&input, &mut gfx, &window)?;
 	}
 
 	Ok(())
 }
 
-struct GameRunner {
+trait Step {
+	fn step(&mut self, input: &Input, gfx: &mut Graphics, window: &Window) -> Result<()>;
+}
+
+struct GameTimerStepper {
 	game: RollyGame,
-	step_mode: bool,
 	update_timer: Timer,
 	draw_timer: Timer,
 }
 
-impl GameRunner {
-	// TODO: two constructors to handle loop or step (or pass in a flag to one)
+impl GameTimerStepper {
 	fn new() -> Self {
 		Self {
 			game: RollyGame::new(),
-			step_mode: false,
 			update_timer: Timer::time_per_second(60.0),
 			draw_timer: Timer::time_per_second(60.0),
 		}
 	}
+}
 
-	pub fn run(
-		&mut self,
-		next_pressed: bool,
-		input: &Input,
-		gfx: &mut Graphics,
-		window: &Window,
-	) -> Result<()> {
-		if self.step_mode {
-			if next_pressed {
-				self.step(&input, gfx, &window)?;
-			}
-		} else {
-			self.timestep(&input, gfx, &window)?;
-		}
-
-		Ok(())
-	}
-
-	fn timestep(&mut self, input: &Input, gfx: &mut Graphics, window: &Window) -> Result<()> {
+impl Step for GameTimerStepper {
+	fn step(&mut self, input: &Input, gfx: &mut Graphics, window: &Window) -> Result<()> {
 		while self.update_timer.tick() {
 			self.game.update(&input);
 		}
@@ -82,12 +67,6 @@ impl GameRunner {
 		}
 
 		Ok(())
-	}
-
-	fn step(&mut self, input: &Input, gfx: &mut Graphics, window: &Window) -> Result<()> {
-		self.game.update(&input);
-		self.game.draw(gfx);
-		gfx.present(&window)
 	}
 }
 
